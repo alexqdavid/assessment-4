c8d78bad71669510823472e996284ec0155c8af9
alexqdavid <alexqdavid@icloud.com>
2022-10-23 13:47:15 -0400

    redo

--- server/controller.js
+++ server/controller.js
@@ -1,3 +1,11 @@
+let game = [
+    {'id': 1, 'newGame': 'Val'},
+    {'id': 2, 'newGame': 'LOL'},
+    {'id': 3, 'newGame': 'Apex'}
+]
+
+
+
 module.exports = {
 
     getCompliment: (req, res) => {
@@ -8,16 +16,49 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
-    }
-
-    
-    // getComplimentOne: (req, res) => {
-    //     const complimentOne = ["A beautiful, smart, and loving person will be coming into your life.",'A dubious friend may be an enemy in camouflage.','A faithful friend is a strong defense.','A fresh start will put you on your way.','A friend is a present you give yourself.'];
-    //     let randomIndex = Math.floor(Math.random() * complimentOne.length);
-    //     let randomCompliment = complimentOne[randomIndex];
+    },
+    getFortune: (req, res) => {
+        const fortunes = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
       
-    //     res.status(200).send(randomCompliment);
-    // }
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+      
+        res.status(200).send(randomFortune);
 
+    },
+    
+    getGames: (res, req) => {
+        res.status(200).json(game);
+    },
+
+
+    postGames: (req, res) => {
+        console.log(req.body);
+
+        const addGames = {
+            id: req.body.id,
+            gameName: req.body.gameName
+           
+        }
+
+        game.push(addGames);
+        res.status(200).json({message: "successful", addGames});
+    },
+
+    editGames: (req, res) => {
+        const {id}= req.params;
+        const updateGame = game.find((objGame)=> objGame.id === parseInt(id))
+        updateGame.gameName=req.body.gameName;
+        res.status(200).json({message: "successful", game});
+
+    },
+
+    deleteGames: (req, res) => {
+        
+        const deleteGames = game.find((objGame) => objGame.id === parseInt(req.params.id));
+        game.splice(req.params.id -1, 1);
+        res.status(200).json({message: "successful", game});
+    }
 }
     
--- server/index.js
+++ server/index.js
@@ -9,18 +9,26 @@ app.use(cors());
 app.use(express.json());
 
 const { 
-    getCompliment,
-    //getComplimentOne,
-    //deleteCompliment,
-    //createCompliment,
-    //updateCompliment
+    getCompliment
  } = require('./controller')
 
-app.get(`/api/compliment`, getCompliment);
-//app.get(`/api/compliment`, getComplimentOne);
-//app.delete(`/api/compliment/:id`, deleteCompliment)
-//app.post(`/api/compliment`, createCompliment)
-//app.put(`/api/compliment/:id`, updateCompliment)
+ const { 
+    getFortune
+ } = require('./controller')
+
+ const { 
+    getGames,
+    postGames,
+    editGames,
+    deleteGames
+ } = require('./controller')
+
+app.get("/api/compliment", getCompliment);
+app.get("/api/compliment", getFortune);
+app.get("/api/games", getGames);
+app.delete("/api/games/:id", deleteGames)
+app.put("/api/games", editGames)
+app.post("/api/games/:id", postGames)
 
 app.listen(5006, () => console.log("Server running on 5006"));
 
